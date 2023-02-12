5481f22a4e52de13d977955dfb20aa4403143c5f
imarcaos <marcaos.melo@gmail.com>
2023-02-12 17:24:01 +0000

    ReactJS Class#012 - Project Reorganization and PascalCase

---
+++ react-components/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './App.css'
+
+// componentes
+import ComponenteUm from "./components/ComponenteUm"
+
+function App() {
+    return (
+        <>
+            <ComponenteUm />
+        </>
+    )
+}
+
+export default App
